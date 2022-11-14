19a7fd56988d420c44aa9900116257a71bd15916
Jaswanth Madiya <jaswanthmadiya@gmail.com>
2022-11-14 07:18:08 +0530

    jhg

--- assistant/open_api.py
+++ assistant/open_api.py
@@ -1,7 +1,18 @@
-def google_search(query):
-    if query is None:
-        return "Some thing went wrong"
-    query = query.replace(' ','+')
-    return (f"https://www.google.com/search?q={query}")
+import openai
 
-print(google_search('who is the prime misniter of india'))
+openai.api_key = "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"
+
+try:
+    query = input()
+    response = openai.Completion.create(
+    engine="text-davinci-002",
+    prompt=query,
+    temperature=0.3,
+    max_tokens=36,
+    top_p=1,
+    frequency_penalty=0.5,
+    presence_penalty=0)
+    print(response.choices[0].text)
+    # print(openai.Model.list())
+except Exception as e:
+    print(f"Error: {e}")
